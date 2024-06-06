aea3524bd4737dea47877a911694e621ae4f0bf7
Kartik Rane <kartiksrane05@gmail.com>
2024-06-06 22:01:34 +0530

    Done for the day with Some class portion left

---
+++ Class/Main.java
@@ -0,0 +1,6 @@
+package Class;
+public class Main {
+    static int Sum(int a, int b){
+        return a +b;
+    }
+}
---
+++ Class/SecondMain.java
@@ -0,0 +1,7 @@
+package Class;
+public class SecondMain {
+   public static void main(String[] args) {
+    Main myobj1 = new Main();
+    System.out.println(myobj1.Sum(5, 5));
+   }
+}
